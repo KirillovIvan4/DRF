879c260dfa792d309bb6606832914d47866fb399
Ivan Kirllov <kirillovio@mail.ru>
2025-05-12 20:17:04 +0300

    Настроил функцию которая делает пользовательй неактивнами если они  не заходили более месяца

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,11 @@ from django.urls import include, path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.http import JsonResponse
 
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -28,6 +32,7 @@ urlpatterns = [
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('health/', health_check),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
