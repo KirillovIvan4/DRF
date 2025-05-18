4e5e1216dfa8d3ed4a33693129a2eb183f153370
Ivan Kirllov <kirillovio@mail.ru>
2025-05-18 21:57:57 +0300

    Исправнения 3в docker

--- users/management/commands/create_test_db.py
+++ users/management/commands/create_test_db.py
@@ -1,16 +1,16 @@
-from django.core.management.base import BaseCommand
-from django.db import connections
-
-
-class Command(BaseCommand):
-    help = 'Creates test database structure'
-
-    def handle(self, *args, **options):
-        """Create test database by cloning the main database"""
-        # Создаем тестовую БД на основе основной
-        with connections['default'].cursor() as cursor:
-            cursor.execute(
-                "CREATE DATABASE test_test_db WITH TEMPLATE test_db OWNER postgres;"
-            )
-
-        self.stdout.write(self.style.SUCCESS('Successfully created test database'))
+# from django.core.management.base import BaseCommand
+# from django.db import connections
+#
+#
+# class Command(BaseCommand):
+#     help = 'Creates test database structure'
+#
+#     def handle(self, *args, **options):
+#         """Create test database by cloning the main database"""
+#         # Создаем тестовую БД на основе основной
+#         with connections['default'].cursor() as cursor:
+#             cursor.execute(
+#                 "CREATE DATABASE test_test_db WITH TEMPLATE test_db OWNER postgres;"
+#             )
+#
+#         self.stdout.write(self.style.SUCCESS('Successfully created test database'))
