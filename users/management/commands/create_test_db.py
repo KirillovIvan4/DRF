560c64eb85ef8744de6813db1c5f1d1b89a82e42
Ivan Kirllov <kirillovio@mail.ru>
2025-05-18 20:30:47 +0300

    Исправнения 13 в test_test

---
+++ users/management/commands/create_test_db.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from django.db import connections
+
+
+class Command(BaseCommand):
+    help = 'Creates test database structure'
+
+    def handle(self, *args, **options):
+        # Создаем тестовую БД
+        with connections['default'].cursor() as cursor:
+            cursor.execute("CREATE DATABASE test_test_db WITH TEMPLATE test_db OWNER postgres;")
+
+        self.stdout.write(self.style.SUCCESS('Successfully created test database'))
